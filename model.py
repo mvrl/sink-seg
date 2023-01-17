fe3429310a47772a17e7581f1fcd4b6ff6646f51
LovingChester <chelsea000719@gmail.com>
2023-01-16 19:24:55 -0600

    modified Unet with fusing technique from fusenet

--- model.py
+++ model.py
@@ -90,7 +90,7 @@ class outconv(nn.Module):
 class Unet(nn.Module):
     """
     This is a standard UNet with minor tweaks. The changes are:
-    - in upsamping, we use nearest neighbor interpolation to avpoid aliasing artifacts
+    - in upsamping, we use nearest neighbor interpolation to avoid aliasing artifacts
     - we have a parameter, feature_reduction, that can be used to use fewer feature maps. When feature_reduction=2, the model will have half the feature maps than the original UNet.
     - We can replace BatchNorm with a norm, such as InstanceNorm.
     """
@@ -103,6 +103,7 @@ class Unet(nn.Module):
         super(Unet, self).__init__()
 
         self.inc = inconv(in_channels, int(64 / feature_reduction))
+
         self.down1 = down(int(64 / feature_reduction),
                           int(128 / feature_reduction), norm_type)
         self.down2 = down(int(128 / feature_reduction),
@@ -123,7 +124,8 @@ class Unet(nn.Module):
 
         self.input_now = []  # just a place holder
 
-    def set_input(self, shaded, dem, naip, dem_dxy, dem_dxy_pre):
+    def set_input(self, shaded, dem, naip, dem_dxy, dem_dxy_pre,
+                  shaded_naip, dem_naip, dem_dxy_naip, dem_dxy_pre_naip):
         """
         Writing this general function to setup input, as specified in the config.py file.
         For different input modalities, only this function will need to be changed. train/eval loops will not be affected.
@@ -146,11 +148,26 @@ class Unet(nn.Module):
         elif cfg.data.input_type == 'dem_dxy_pre':
             input_now = dem_dxy_pre
 
+        # new added
+        elif cfg.data.input_type == 'shaded_relief_naip':
+            input_now = shaded_naip
+
+        elif cfg.data.input_type == 'dem_naip':
+            input_now = dem_naip
+
+        elif cfg.data.input_type == 'dem_derivative_naip':
+            input_now = dem_dxy_naip
+
+        elif cfg.data.input_type == 'dem_dxy_pre_naip':
+            input_now = dem_dxy_pre_naip
+
         return input_now
 
-    def forward(self, shaded, dem, naip, dem_dxy, dem_dxy_pre):
+    def forward(self, shaded, dem, naip, dem_dxy, dem_dxy_pre,
+                shaded_naip, dem_naip, dem_dxy_naip, dem_dxy_pre_naip):
         # set appropriate input
-        input_now = self.set_input(shaded, dem, naip, dem_dxy, dem_dxy_pre)
+        input_now = self.set_input(shaded, dem, naip, dem_dxy, dem_dxy_pre,
+                                   shaded_naip, dem_naip, dem_dxy_naip, dem_dxy_pre_naip)
 
         x1 = self.inc(input_now)
         x2 = self.down1(x1)
@@ -162,4 +179,115 @@ class Unet(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         x = self.outc(x)
+        return x
+
+
+# combine fusion technique in FuseNet with Unet
+class Unet_early(nn.Module):
+    def __init__(self,
+                 in_channels=3,
+                 out_channels=1,
+                 feature_reduction=1,
+                 norm_type='batch'):
+        super(Unet_early, self).__init__()
+
+        self.dem_down1 = double_conv(
+            in_channels-4, int(64 / feature_reduction))
+        self.naip_down1 = double_conv(4, int(64 / feature_reduction))
+
+        self.dem_down2 = double_conv(
+            int(64 / feature_reduction), int(128 / feature_reduction))
+        self.naip_down2 = double_conv(
+            int(64 / feature_reduction), int(128 / feature_reduction))
+
+        self.dem_down3 = double_conv(
+            int(128 / feature_reduction), int(256 / feature_reduction))
+        self.naip_down3 = double_conv(
+            int(128 / feature_reduction), int(256 / feature_reduction))
+
+        self.dem_down4 = double_conv(
+            int(256 / feature_reduction), int(512 / feature_reduction))
+        self.naip_down4 = double_conv(
+            int(256 / feature_reduction), int(512 / feature_reduction))
+
+        self.dem_down5 = double_conv(
+            int(512 / feature_reduction), int(512 / feature_reduction))
+        self.naip_down5 = double_conv(
+            int(512 / feature_reduction), int(512 / feature_reduction))
+
+        self.up1 = up(int(1024 / feature_reduction),
+                      int(256 / feature_reduction), norm_type)
+        self.up2 = up(int(512 / feature_reduction),
+                      int(128 / feature_reduction), norm_type)
+        self.up3 = up(int(256 / feature_reduction), int(64 / feature_reduction),
+                      norm_type)
+        self.up4 = up(int(128 / feature_reduction), int(64 / feature_reduction),
+                      norm_type)
+        self.outc = outconv(int(64 / feature_reduction), out_channels)
+
+    def set_input(self, shaded, dem, naip, dem_dxy, dem_dxy_pre):
+        """
+        Writing this general function to setup input, as specified in the config.py file.
+        For different input modalities, only this function will need to be changed. train/eval loops will not be affected.
+        """
+
+        # new added
+        if cfg.data.input_type == 'shaded_relief_naip':
+            input_now = [shaded, naip]
+
+        elif cfg.data.input_type == 'dem_naip':
+            input_now = [dem, naip]
+
+        elif cfg.data.input_type == 'dem_derivative_naip':
+            input_now = [dem_dxy, naip]
+
+        elif cfg.data.input_type == 'dem_dxy_pre_naip':
+            input_now = [dem_dxy_pre, naip]
+
+        return input_now
+
+    def forward(self, shaded, dem, naip, dem_dxy, dem_dxy_pre):
+
+        # set appropriate input
+        input_now = self.set_input(shaded, dem, naip, dem_dxy, dem_dxy_pre)
+
+        if input_now[0].shape[2] != input_now[1].shape[2]:
+            diff = abs(input_now[0].shape[2]-input_now[1].shape[2])
+            pad = nn.ZeroPad2d((0, 0, 0, diff))
+            input_now[0] = pad(input_now[0])
+
+        x1_dem = self.dem_down1(input_now[0])
+        x1_naip = self.naip_down1(input_now[1])
+        x1 = x1_dem + x1_naip
+        x1_dem = F.max_pool2d(x1_dem, kernel_size=2)
+        x1_f = F.max_pool2d(x1, kernel_size=2)
+
+        x2_dem = self.dem_down2(x1_dem)
+        x2_naip = self.naip_down2(x1_f)
+        x2 = x2_dem + x2_naip
+        x2_dem = F.max_pool2d(x2_dem, kernel_size=2)
+        x2_f = F.max_pool2d(x2, kernel_size=2)
+
+        x3_dem = self.dem_down3(x2_dem)
+        x3_naip = self.naip_down3(x2_f)
+        x3 = x3_dem + x3_naip
+        x3_dem = F.max_pool2d(x3_dem, kernel_size=2)
+        x3_f = F.max_pool2d(x3, kernel_size=2)
+
+        x4_dem = self.dem_down4(x3_dem)
+        x4_naip = self.naip_down4(x3_f)
+        x4 = x4_dem + x4_naip
+        x4_dem = F.max_pool2d(x4_dem, kernel_size=2)
+        x4_f = F.max_pool2d(x4, kernel_size=2)
+
+        x5_dem = self.dem_down5(x4_dem)
+        x5_naip = self.naip_down5(x4_f)
+        x5 = x5_dem + x5_naip
+
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+
         return x
